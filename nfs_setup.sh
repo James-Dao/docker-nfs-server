3f39ab13c3f7d6a2f260e9c694b4e98f336f8bd9
James-Dao <James.Xiong@daocloud.io>
2016-09-14 03:54:05 +0800

    Update nfs_setup.sh

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -5,7 +5,7 @@ set -e
 for mnt in "$@"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   mkdir -p $src
-  echo "$src *(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
+  echo "$src *(rw,sync,no_subtree_check)" >> /etc/exports
 done
 
 exec runsvdir /etc/sv

8e49560d68f8ee8789ab1079955d556ef9a2d349
James-Dao <James.Xiong@daocloud.io>
2016-09-14 02:46:26 +0800

    Update nfs_setup.sh

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -6,7 +6,6 @@ mounts="${@}"
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
-  mkdir -p $src
   echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
 done
 

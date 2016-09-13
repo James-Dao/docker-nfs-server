a7056ebe2d944db082600d4e089c18e5e34d0eb7
James-Dao <James.Xiong@daocloud.io>
2016-09-14 03:43:54 +0800

    Update nfs_setup.sh

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-mounts="${@}"
-
-for mnt in "${mounts[@]}"; do
+for mnt in "$@"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   mkdir -p $src
   echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
